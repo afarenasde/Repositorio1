bb3f76a819599792e34913dd728f98ed60456332
Guillermo Niño <gninoo@dian.gov.co>
2024-05-15 12:19:35 -0500

    MENSAJE

--- src/PartsUnlimited.Models/Category.cs
+++ src/PartsUnlimited.Models/Category.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    // nuevo cambio de CATEGORY
     public class Category
     {
         public int CategoryId { get; set; }
